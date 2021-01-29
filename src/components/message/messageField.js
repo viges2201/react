cfa9525fdeaf5a96d98a347f18eb3b6c4b381fbd
viges2201 <viges2201@hotmail.com>
2021-01-29 10:40:14 +0300

    create fuctionsl handleAddMessage

--- src/components/message/messageField.js
+++ src/components/message/messageField.js
@@ -15,41 +15,60 @@ export default function MessageField ({chatId}) {
     2: { text: "Здравствуйте!", sender: 'bot' },
   });
 
-  const handleAddMessage = useCallback((text, author='me', id = Date.now()) => {
-    setMessages((oldMessages) => ([...oldMessages, { id, text, author}]));
-  },[]); // Пока не получилось сделать
-
-  return (
-    <div className="layout">
-      <div className="message-field">
-        {chats[chatId].messageList.map((messageId, index) => (
-          <Message
-            key={ index }
-            text={ messages[messageId].text }
-            sender={ messages[messageId].sender }
-            />))}
-          </div>
-          <MessageInput onAddMessage = {handleAddMessage}></MessageInput>
-        </div>
-      )
+  const handleAddMessage = useCallback((message, sender='me') => {
+    const messageId = Object.keys(messages).length + 1;
+    setMessages((oldMessages) => ({...oldMessages,[messageId] : { text: message, sender}}));
+    setChats((oldChats) => ({...chats,
+      [chatId]: { ...chats[chatId],
+        messageList: [...chats[chatId]['messageList'], messageId]
+      }
     }
+  ));
+},[messages, chatId, chats]);
+
+useEffect(()=> {
+  let timeout;
+  if (Object.values(messages)[Object.values(messages).length - 1].sender === 'me') {
+    timeout = setTimeout(() =>
+    handleAddMessage('Не приставай ко мне, я робот!', 'bot'), 1000);
+  }
+
+  return () => {
+    clearTimeout(timeout);
+  }
+}, [messages, handleAddMessage]);
+
+return (
+  <div className="layout">
+    <div className="message-field">
+      {chats[chatId].messageList.map((messageId, index) => (
+        <Message
+          key={ index }
+          text={ messages[messageId].text }
+          sender={ messages[messageId].sender }
+          />))}
+        </div>
+        <MessageInput onAddMessage = {handleAddMessage}></MessageInput>
+      </div>
+    )
+  }
 
 
-    // const  renderMessage = useCallback((chatsmessages) => {
-    //   return (
-    //     <Message key = {messages.id}  text = {messages.text} sender = {messages.sender}></Message>
-    //   )
-    // }, []);
+  // const  renderMessage = useCallback((chatsmessages) => {
+  //   return (
+  //     <Message key = {messages.id}  text = {messages.text} sender = {messages.sender}></Message>
+  //   )
+  // }, []);
 
-    // useEffect(()=> {
-    //   let timeout;
-    //   if (messages[messages.length - 1].sender !== 'robot') {
-    //     timeout = setTimeout(() => {
-    //       handleAddMessage('Привет, я робот!', 'robot')
-    //     }, 1000);
-    //   }
-    //
-    //   return () => {
-    //     clearTimeout(timeout);
-    //   }
-    // }, [messages, handleAddMessage])
+  // useEffect(()=> {
+  //   let timeout;
+  //   if (messages[messages.length - 1].sender !== 'robot') {
+  //     timeout = setTimeout(() => {
+  //       handleAddMessage('Привет, я робот!', 'robot')
+  //     }, 1000);
+  //   }
+  //
+  //   return () => {
+  //     clearTimeout(timeout);
+  //   }
+  // }, [messages, handleAddMessage])
