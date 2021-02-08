f010f68a2ab101ca118deba45b2d5261b36b3f85
viges2201 <viges2201@hotmail.com>
2021-02-08 19:32:43 +0300

    Dz lesson 7 Пытался сделать по методичке с выгрузкой из локальной папки, не получилось, пробую настроить и запустить локальный сервер

--- src/actions/chatActions.js
+++ src/actions/chatActions.js
@@ -1,6 +1,32 @@
-export const ADD_CHAT = '@@chat/ADD_CHAT';
+import { RSAA, getJSON } from 'redux-api-middleware';
+import { normalize } from 'normalizr';
+import { chats } from '../utils/schemas';
+
+export const ADD_CHAT = '@@chat/SEND_MESSAGE';
 
 export const addChat = (title) => ({
    type: ADD_CHAT,
    title,
+});
+
+
+export const START_CHATS_LOADING = '@@chat/START_CHATS_LOADING';
+export const SUCCESS_CHATS_LOADING = '@@chat/SUCCESS_CHATS_LOADING';
+export const ERROR_CHATS_LOADING = '@@chat/ERROR_CHATS_LOADING';
+
+export const loadChats = () => ({
+   [RSAA]: {
+       endpoint: '/api/chats.json',
+       method: 'GET',
+       types: [
+           START_CHATS_LOADING,
+           {
+               type: SUCCESS_CHATS_LOADING,
+               payload: (action, state, res) => getJSON(res).then(
+                   json => normalize(json, [chats]),
+               ),
+           },
+           ERROR_CHATS_LOADING,
+       ],
+   },
 });
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,12 @@
-import { combineReducers } from 'redux';
+// reducers.js
+import { combineReducers } from 'redux'
 import { connectRouter } from 'connected-react-router'
 import chatReducer from './chatReducer';
 import messageReducer from './messageReducer';
 
-
-export default (history) => combineReducers({
-   router: connectRouter(history),
-   chatReducer,
-   messageReducer,
-});
+const createRootReducer = (history) => combineReducers({
+  router: connectRouter(history),
+  chatReducer,
+  messageReducer,
+})
+export default createRootReducer
